6633317f79bef92bc955b043bb90627fe1926b4e
Felipe Silva <felype.hen@hotmail.com>
2022-10-14 11:59:21 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+      <h1 className="text-xl font-bold text-violet-700 bg-cyan-500">Hello World</h1>
+    </>
+   
+  )
+}

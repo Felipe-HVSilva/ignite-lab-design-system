ff04c9002ece9dd1ef9ae0f5fe475815765c8a30
Felipe Silva <felype.hen@hotmail.com>
2022-10-14 13:30:24 -0300

    Fix All v7

--- storybook-static/assets/preview.14562985.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.8d542027.js";import"./iframe.a8f9268b.js";import"./es.map.constructor.1d952e29.js";import"./es.number.to-fixed.c3c058c8.js";import"./index.92dffc41.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.14562985.js.map
